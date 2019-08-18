2f118593be9f5d652b441a26b00229a033402647
Rijnard van Tonder <rvantonder@gmail.com>
2019-08-17 23:14:03 -0400

    add dump script

---
+++ scripts/dump.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set -ex
+set -e
+
+MERLIN_BINARY="Users/rvt/merlin/ocamlmerlin"
+
+EXCLUDE="_build\|test"
+
+FILES=$(find . -type f \( -name "*.ml" -o -name "*.mli" \) | grep -v "$EXCLUDE")
+N_FILES=$(echo "$FILES" | wc -l)
+i=0
+
+for f in $FILES; do
+  FILE_DIR=$(dirname $f)
+  if [[ -f "$FILE_DIR/.merlin" ]]; then
+    ((i++))
+    printf "(%4d/%4d) %s\n" "$i" "$N_FILES" "$f"
+    cat "$f" | $MERLIN_BINARY server lsif "$f" "-dot-merlin" "$FILE_DIR/.merlin" > "$f.lsif" # &
+  fi
+done
+
+# wait

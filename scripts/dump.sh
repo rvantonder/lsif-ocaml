454a603a8961361ff03ae24855d67be27b943a35
Rijnard van Tonder <rvantonder@gmail.com>
2019-08-28 11:48:18 -0400

    update dump script

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -9,8 +9,6 @@ function ctrl_c() {
 
 trap ctrl_c INT
 
-PARALLEL_FRENZY=$1
-
 EXISTS=$(command -v ocamlmerlin-with-lsif || echo)
 
 if [ ! -n "$EXISTS" ]; then
@@ -22,6 +20,30 @@ fi
 
 EXCLUDE="_build\|test"
 
+while test $# -gt 0; do
+    case "$1" in
+        "-p") 
+            shift
+            PARALLEL_FRENZY="1"
+            ;;
+        "-h"|"--help"|"-help") echo -e "-exclude \"pattern\": exclude paths containing this regex pattern (\"_build|test\" is the default)\n-p: parallel frenzy (spawn a bunch of processes in parallel)\n-h: this help menu"; exit 0;;
+        "-exclude") 
+            shift
+            if test $# -gt 0; then
+                EXCLUDE=$1
+                EXCLUDE=$(echo "$EXCLUDE" | sed 's/|/\\|/g')
+            else
+                echo "No pattern to exclude"
+                exit 1
+            fi
+            shift
+            ;;
+        *) 
+            break
+            ;;
+    esac
+done
+
 FILES=$(find . -type f \( -name "*.ml" -o -name "*.mli" \) | grep -v "$EXCLUDE")
 N_FILES=$(echo "$FILES" | wc -l)
 i=0
@@ -32,7 +54,7 @@ for f in $FILES; do
     ((i++))
     printf "(%4d/%4d) %s\n" "$i" "$N_FILES" "$f"
    if [ -z "$PARALLEL_FRENZY" ]; then
-      cat "$f" | $MERLIN_LSIF_BINARY server lsif "$f" "-dot-merlin" "$FILE_DIR/.merlin" > "$f.lsif.in" # &
+      cat "$f" | $MERLIN_LSIF_BINARY server lsif "$f" "-dot-merlin" "$FILE_DIR/.merlin" > "$f.lsif.in"
     else
       cat "$f" | $MERLIN_LSIF_BINARY server lsif "$f" "-dot-merlin" "$FILE_DIR/.merlin" > "$f.lsif.in" 2> /dev/null &
     fi
